eea8424fca6d1b0202f7346b48a5540df97a22fc
swarna1993 <swarna.krisanya@gmail.com>
2022-08-18 12:53:41 +0530

    PBA-6:create controller class

---
+++ src/main/java/com/bikkadIt/PhoneBookApp/controller/ContactController.java
@@ -0,0 +1,12 @@
+package com.bikkadIt.PhoneBookApp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bikkadIt.PhoneBookApp.service.ServiceImpl;
+
+@RestController
+public class ContactController {
+	
+	private ServiceImpl serviceImpl;
+
+}
