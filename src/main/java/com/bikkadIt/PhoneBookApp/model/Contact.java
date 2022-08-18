d59e4bdb8d3b63eb86332c0d6544db2b409e9907
swarna1993 <swarna.krisanya@gmail.com>
2022-08-18 12:09:54 +0530

    PBA-3:Entity class created

---
+++ src/main/java/com/bikkadIt/PhoneBookApp/model/Contact.java
@@ -0,0 +1,102 @@
+package com.bikkadIt.PhoneBookApp.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CONTACT_DTLS")
+public class Contact {
+	
+	@Id
+	@Column(name="CONTACT_ID")
+	private Integer contactId;
+	
+	@Column(name="CONTACT_NAME")
+	private String contactName;
+	
+	@Column(name="CONTACT_NUMBER")
+	private String contactNumber;
+	
+	@Column(name="CONTACT_EMAIL")
+	private String contactEmail;
+	
+	@Column(name="ACTIVE_SWITCH")
+	private char activeSwitch;
+	
+	@Column(name="CREATED_DATE")
+	private LocalDate createdDate;
+	
+	@Column(name="UPDATED_DATE")
+	private LocalDate  updatedDate;
+
+	public Integer getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(Integer contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public char getActiveSwitch() {
+		return activeSwitch;
+	}
+
+	public void setActiveSwitch(char activeSwitch) {
+		this.activeSwitch = activeSwitch;
+	}
+
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDate getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDate updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [contactId=" + contactId + ", contactName=" + contactName + ", contactNumber=" + contactNumber
+				+ ", contactEmail=" + contactEmail + ", activeSwitch=" + activeSwitch + ", createdDate=" + createdDate
+				+ ", updatedDate=" + updatedDate + "]";
+	}
+	
+	
+
+}
+
